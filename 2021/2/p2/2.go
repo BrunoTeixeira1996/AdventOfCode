f7c698aac465ae16d87e11d0eecc3c444b790518
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2021-12-02 22:26:09 +0000

    second challenge done

---
+++ 2021/2/p2/2.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func read_file(file string, sol *map[string]int) {
+	f, err := os.Open(file)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+
+	for s.Scan() {
+		words := strings.Fields(s.Text())
+		value, _ := strconv.Atoi(words[1])
+		if words[0] == "forward" {
+			(*sol)["horizontal_pos"] += value
+			(*sol)["depth"] += (*sol)["aim"] * value
+		} else if words[0] == "down" {
+			(*sol)["aim"] += value
+		} else {
+			(*sol)["aim"] -= value
+		}
+	}
+
+}
+
+func count_part_two(sol map[string]int) int {
+	anser := sol["horizontal_pos"] * sol["depth"]
+	return anser
+
+}
+
+func main() {
+	sol := map[string]int{
+		"horizontal_pos": 0,
+		"depth":          0,
+		"aim":            0,
+	}
+
+	read_file("input", &sol)
+	anser := count_part_two(sol)
+	fmt.Println(anser)
+}
