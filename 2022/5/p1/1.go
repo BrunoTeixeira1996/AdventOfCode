d840b7275e9d7bccb4bbe59713d5e4bfb5b738a5
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2022-12-06 15:30:10 +0000

    working on day 5 still

--- 2022/5/p1/1.go
+++ 2022/5/p1/1.go
@@ -11,8 +11,6 @@ import (
 )
 
 var IsLetter = regexp.MustCompile(`^[A-Z]+$`).MatchString
-
-// var IsLower = regexp.MustCompile(`^[a-z]+$`).MatchString
 var IsNumber = regexp.MustCompile(`\d`).MatchString
 
 type Stack struct {
@@ -84,66 +82,67 @@ func read_file(file string) ([]Stack, []Move) {
 	return res, moves
 }
 
-func structStack(stack []Stack) {
-	/*
-	   map[1:[{N 1} {Z 1}] 5:[{D 5} {C 5} {M 5}] 9:[{P 9}]]
-	*/
+func normalizeStack(temp *[][]Stack) {
+    stackLen := 1
 
+    for _, s := range *temp {
+        for i := range s {
+            s[i].Pos = stackLen
+        }
+        stackLen += 1
+    }
+
+}
+
+func structStack(stack []Stack) [][]Stack {
 	sort.Slice(stack[:], func(i, j int) bool {
 		return stack[i].Pos < stack[j].Pos
 	})
 
-	groupStack := make(map[int][]Stack)
-	for _, b := range stack {
-		groupStack[b.Pos] = append(groupStack[b.Pos], b)
-	}
+	// normalize slice with correct indexes
+	count := 0
+	var temp [][]Stack
+	var aux []Stack
 
-	var v [][]Stack
-	for _, value := range groupStack {
-		v = append(v, value)
-	}
-
-    normalizeMap(groupStack)
-}
-
-// TODO: normalize this to do the moveStack function
-func normalizeMap(groupStack map[int][]Stack) {
-    count := 1
-    for index := range groupStack{
-        fmt.Printf("Count: %d Index: %d\n", count, index)
+    aux = append(aux, stack[0])
+    
+    for _ , value := range stack {
+        if count > 0 {
+            if value.Pos == aux[len(aux)-1].Pos {
+                aux = append(aux, value)
+            } else {
+                temp = append(temp, aux)
+                aux = nil
+                aux = append(aux, value)
+             }
+        }
         count += 1
-
     }
-    fmt.Println(groupStack)
+    temp = append(temp, aux)
+
+    normalizeStack(&temp)
+    return temp
 }
 
+// TODO: THIS
+func moveStacks(move Move, stacks [][]Stack) {
+    //fmt.Printf("%#v\n", stacks[move.Source - 1][move.Size - 1]) // move 1 from 2 -> stacks[move.Source - 1][move.Size - 1]
+    //fmt.Printf("%#v\n", stacks[move.Dest - 1]) // to 1 -> stacks[move.Dest - 1]
+    
+    stacks[move.Dest - 1] = append([]Stack{stacks[move.Source - 1][move.Size - 1]}, stacks[move.Dest - 1]...) // appends to the head
 
-func moveStacks(move Move, stacks []Stack) {
-    /*
-    Find quantity to move -> move.Size
-    Find source -> move.Source
-    Find destination -> move.Destination
-    */
-    var temp []string
-    for _ , i := range stacks {
-        
-        temp = append(temp, i.Letter) // append to temp the moved element
-        i.Pos = move.Dest // changed the pos of that object
-        //remove() // pop the element that was moved
-        
-    } 
-        // append temp to destination stack
+    stacks[move.Source - 1] = stacks[move.Source - 1][1:] // remove  from moved stack // THIS IS WRONG
 
+    normalizeStack(&stacks) // after moving the stack, normalize again
 }
 
 func main() {
 	stack, moves := read_file("input")
-	structStack(stack)
-    /*
-    for _, i := range moves {
-        moveStacks(i, stack)
-        break
-    } 
-    */
-    fmt.Println(moves)
+	stacks := structStack(stack)
+
+    for _, move := range moves {
+        moveStacks(move, stacks)
+        fmt.Println(stacks)
+    }
+
 }
